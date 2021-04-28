b84b19f7af403f37c0cf05218d2a3fc04d95ef33
rukesh <rukesh@rukesh>
2021-04-28 09:31:52 +0530

    change value to 6

--- listdemo/src/com/scg/main/Execute.java
+++ listdemo/src/com/scg/main/Execute.java
@@ -7,7 +7,7 @@ public class Execute {
 
 	public static void main(String[] args) {
 		List<Employee> employee = new ArrayList<Employee>();
-		int employee1=1;
+		int employee1=6;
 		
 		employee.add(new Employee(3, "Rukesh","Developer","Bangalore",25000));
 		employee.add(new Employee(4, "Ravichandra","Tester","Bangalore",22000));
